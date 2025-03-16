812af46c1bacdfa72256b908f1b510ef44af69de
gdhhkkl <lee040319ww@naver.com>
2025-03-16 16:22:30 +0900

    petShop

---
+++ week01/gdhhkkl/Beauty.java
@@ -0,0 +1,8 @@
+package org.example.Pet;
+
+public interface Beauty {
+    public int takeTheShower(String kind,int dogFur);
+    public int takeTheDry(int dongFur);
+    public int takeTheBrush(int dogFur);
+    public int cutTheScissors(int dogFur);
+}
